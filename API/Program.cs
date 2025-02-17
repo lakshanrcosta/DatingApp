fb79719f0960c2deac1cbde4370a7056eff4a4ee
Lakshan Costa <lakshanrcosta@gmail.com>
2025-02-17 18:15:03 +0200

    Cleaned up Program.cs and added extensions for Identity and Application configs

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,10 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DatabaseContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -22,6 +16,9 @@ app.UseCors(builder =>
         .AllowAnyHeader()
         .WithOrigins("http://localhost:4200", "https://localhost:4200")
 );
+
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
