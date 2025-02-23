033f51c21e5b34dd29c9e2d303dc41693c179af9
Lakshan Costa <lakshanrcosta@gmail.com>
2025-02-24 00:12:00 +0200

    Created login popup dialog box component

---
+++ client/src/app/components/login-dialog/login-dialog.component.ts
@@ -0,0 +1,59 @@
+import { Component, inject } from '@angular/core';
+import { MaterialModule } from '../../shared/material.module';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { AccountService } from '../../services/account/account.service';
+import { LoginDto } from '../../dtos/login.dto';
+
+@Component({
+  selector: 'app-login-dialog',
+  standalone: true,
+  imports: [MaterialModule, CommonModule],
+  templateUrl: './login-dialog.component.html',
+  styleUrl: './login-dialog.component.scss'
+})
+export class LoginDialogComponent {
+  loginForm: FormGroup;
+  private accountService = inject(AccountService);
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private dialogRef: MatDialogRef<LoginDialogComponent>
+  ) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const loginDto: LoginDto = {
+        username: this.loginForm.value.username,
+        password: this.loginForm.value.password
+      };
+
+      this.accountService.login(loginDto).subscribe({
+        next: (response) => {
+          this.dialogRef.close(response);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    }
+  }
+
+  signup() {
+    console.log('Redirect to Sign Up Page');
+  }
+
+  forgotPassword() {
+    console.log('Redirect to Forgot Password Page');
+  }
+}
