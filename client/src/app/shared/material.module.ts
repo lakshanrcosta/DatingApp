f04b6d1f26302d68787fe15f53b95c2ab1f4ea04
Lakshan Costa <lakshanrcosta@gmail.com>
2025-02-24 00:14:59 +0200

    Created material module for import

---
+++ client/src/app/shared/material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+// Import Angular Material Components
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule
+  ]
+})
+export class MaterialModule {}
