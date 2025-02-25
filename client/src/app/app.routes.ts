635956c671b450e3ea4ab0591597ff08716b2e7d
Lakshan Costa <lakshanrcosta@gmail.com>
2025-02-25 21:57:09 +0200

    Added signup route and home route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signup', component: SignupComponent }
+];
